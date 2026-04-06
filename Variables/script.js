ae96ee46c614fcfa7dbc2e9ccc572fdf1a76e1d0
Hammad <tanolihammad14@gmail.com>
2026-04-06 14:41:34 +0500

    Variables

---
+++ Variables/script.js
@@ -0,0 +1,63 @@
+// Variables
+
+// String Variable
+
+let name = "Hammad Siddique"
+console.log(name);
+
+// Number with const mean that can't be changed in future
+
+const age = 23
+console.log(age);
+
+// value change with const
+
+const city = "karachi"
+// city  = "Lahore"
+console.log(typeof city, city);
+
+// Boolean mean true or false and the value can't be in quotes
+
+let student = true
+console.log(student);
+
+
+// Undefined mean the value is not defined
+
+
+let married;
+console.log(married);
+
+// Null mean ke value khud nahi dali
+
+let vichle = null;
+console.log(vichle);
+
+
+// Variables with typeof
+
+// String Variable with typeof
+
+let nam = "Hammad Siddique"
+console.log(typeof nam);
+
+// Number with const mean that can't be changed in future  with typeof
+
+const ages = 23
+console.log(typeof ages);
+
+
+// Boolean mean true or false and the value can't be in quotes with typeof
+
+let students = true
+console.log(typeof students);
+
+// Null mean ke value khud nahi dali with typeof
+
+let vichles = null;
+console.log(typeof vichles);
+
+
+
+
+// with type of when i print the same variable i got problem that you can't declare the same variable so i just change the name and solve the problem

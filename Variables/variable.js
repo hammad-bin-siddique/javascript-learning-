a98844c1d39e93610cd6ed325e870ed47b3b665e
Hammad <tanolihammad14@gmail.com>
2026-04-06 17:23:25 +0500

    Practice of Variable

---
+++ Variables/variable.js
@@ -0,0 +1,64 @@
+
+// !Const value can't be changed
+
+const accountId = 124215
+
+// accountId = 5343455  mean if we forced to change it will not change
+
+console.log(accountId);
+
+
+// ! We can change the value later
+
+let city = "Karachi"
+
+city = "Abbottabad"
+
+console.log(city);
+
+// ! with Typeof
+
+// string
+
+
+let name = "Fahad"
+// console.log( typeof name, name);
+
+// string
+const accountEmail = "tanolihammad14@gmail.com"
+console.log(typeof accountEmail, accountEmail);
+
+
+// number 
+
+let age = 19
+age = 20
+// console.log(typeof age, age);
+
+// Boolean
+
+
+let married = false
+married = true
+console.log(typeof married, married);
+
+// with const 
+
+const vichle = true
+// vichle = false      here it's not change because of const and make the declaration wrong 
+console.log(typeof vichle, vichle);
+
+
+// let's play with let variables 
+
+
+age = 30
+console.log(typeof age, age);
+
+name = "Hammad Siddique"
+
+console.log(typeof name, name);
+
+let password = 123413
+password = "hammad1234"
+console.log(typeof password, password);

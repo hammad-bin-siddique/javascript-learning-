fa6dd61ee48bc39b980742447b1ad43c439afd81
Hammad <tanolihammad14@gmail.com>
2026-06-28 22:53:30 +0500

    Queue Core Operation

---
+++ Data-Structure/Queue /queuecoreoperation.js
@@ -0,0 +1,24 @@
+//! Phase 2 Start — Topic 6: enqueue() 🚀
+
+
+// 📝 Sabse Simple Tarika — Array Se
+// JavaScript mein array ko Queue ki tarah use kar sakte hain:
+
+let queue = [];
+
+queue.push("Hammad");
+queue.push("Fahad");
+queue.push("Ali");
+
+console.log(queue); // yaha hammad fahad ali ai ga 
+
+
+let routeQueue = [];
+
+routeQueue.push("Multan");
+routeQueue.push("Islamabad");
+routeQueue.push("Rawalpindi");
+console.log(routeQueue); // multan islamabad rawalpindi
+routeQueue.push("Lahore");
+
+console.log(routeQueue); 

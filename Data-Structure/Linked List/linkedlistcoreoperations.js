96e2ad4829303f930ae6aa79fef169ffa3108909
Hammad <tanolihammad14@gmail.com>
2026-07-22 17:21:13 +0500

    Remove From Start && Remove From End Method

--- Data-Structure/Linked List/linkedlistcoreoperations.js
+++ Data-Structure/Linked List/linkedlistcoreoperations.js
@@ -40,7 +40,7 @@ class LinkedList {
     this.length++;
   }
 
-  // ? prepand method banai ge mean ke ab agar koi aisa banda a jata ha jis ko importance deni or sab se agey karna ha to kese ho ga
+  // ? phase 3 topic 4 prepand method banai ge mean ke ab agar koi aisa banda a jata ha jis ko importance deni or sab se agey karna ha to kese ho ga
 
   prepend(data) {
     const newNode = new Node(data);
@@ -56,7 +56,7 @@ class LinkedList {
     this.length++;
   }
 
-  //? ab insert function banai ge mean ke agar beach main kisi index per koi new node link karna ho to kese kare ge
+  //? Phase 3 topic 5 ab insert function banai ge mean ke agar beach main kisi index per koi new node link karna ho to kese kare ge
 
   insert(index, data) {
     //  Case 1 agar index 0 ha to waha beach main add nahi kar sakte is lia waha prepend method use kare ge
@@ -92,8 +92,68 @@ class LinkedList {
     previousNode.next = newNode;
     this.length++;
 
-  }
+  };
 
+  //? Phase 3 — Topic 6: Deletion — removeFromStart() is topic main ham linked list ke start se item ko nikale ge or us se agle ko head bana de ga 
+
+  removeFromStart() {
+    if(this.head === null) {
+      console.log("Head is Empty");
+      return null;
+
+      // agar head pehle se he khali ha to return karo  null or console message bhi kar do ke Head is empty 
+    };
+
+    const removeNode = this.head; // head ko remove kar do 
+    this.head = this.head.next; // ab head se agle wale ko head bana do 
+
+    if(this.head === null) {
+      this.tail = null;
+    };
+
+    this.length--;
+
+    return removeNode.item;
+  };
+
+  //? Phase 3 — Topic 7: removeFromEnd() is case main sab se last se banda nikle ga 
+
+
+  removeFromEnd() {
+    // Case 1 Agar head khali ho to 
+    if(this.head === null) {
+      console.log("Head is Empty");
+      return null;
+    };
+
+    // Case 2 Agar head or tail dono 1 he node ho to 
+
+    if(this.head === this.tail) {
+
+      const removeNode = this.head;
+      this.head = null;
+      this.tail = null;
+      this.length--;
+      return removeNode.item;
+    };
+
+    // Case 3 agar 2 ya ziada node ho to phir kia karna ha 
+
+    let current = this.head; // head ko current variable main rakho reference ke lia 
+
+    while (current.next.next !== null) {
+      current = current.next; 
+      //  yani jab tak current ke next ka next null nahi hota tab tak loop chalao or current ke jaga badalte jao man lo 10 20 30  40 null ha current abhi 10 ha 20 1 step phir 30 2 step  us ke bad null nahi ha to loop chalao mean ke 40 ha loop chale ga or 20 per abhi current a jai ga is tara loop chalta jai ga jab tak condition match nahi ho jati 
+    };
+
+    const removeNode = current.next;
+    current.next = null;
+    this.tail = current;
+    this.length--;
+    return removeNode.item;
+
+
+  };
 }
 
 const items = new LinkedList();
@@ -101,10 +161,29 @@ items.append(20);
 items.append(30); // tail
 items.prepend(10); // ye akhir main aya ha but is ko importance di ha is lia ye head ho ga
 items.insert(2, 25); // index 2 pe 25 daalna hai
+console.log(`Removed From Start ${items.removeFromStart()}`); // yaha se ab 10 nikal gia
+
+console.log(items.head.item); // 20
+console.log(items.head.next.item); // 25
+console.log(items.head.next.next.item); // 30 
+//! console.log(items.head.next.next.next.item);
+//yaha per error ai ga keuke 25 ka next 30 ha or 30 ka next null ha or ham null mian item ko dhond rahe ha or javascript null ke andar ja he nahi sakta ke wo waha property ka access kare is lia error ai ga 
+console.log(items.length); // 3  10 nikal gia
 
 
-console.log(items.head.item); // 10 
-console.log(items.head.next.item); // 20
-console.log(items.head.next.next.item); // 25 
-console.log(items.head.next.next.next.item); // 30
-console.log(items.length); // 4
+console.log("====Remove from First and last===");
+
+const list = new LinkedList();
+list.append(10); 
+list.append(20);
+list.append(30);
+list.prepend(40); 
+console.log(list.length); 4
+console.log(list.removeFromEnd()); // 30 gia
+console.log(list.head.item); // 40
+console.log(list.tail.item); // 20
+console.log(list.length); // 3
+console.log(list.removeFromStart()); // ab 40 gia 
+console.log(list.head.item); // 10
+console.log(list.tail.item); // 20
+console.log(list.length); // 2

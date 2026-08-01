b2217eeb6887665acbdd098a257dba02c2f5a0ce
Hammad <tanolihammad14@gmail.com>
2026-08-01 16:02:49 +0500

    Detect Cycle Method, Remove Duplicate Nodes from Linked list Using Set

--- Data-Structure/Linked List/linkedlistpractice.js
+++ Data-Structure/Linked List/linkedlistpractice.js
@@ -64,7 +64,58 @@ class LinkedList {
     }
 
     return slow.item;
-  }
+  };
+
+  //! Topic 3: Detect Cycle in Linked List (Floyd's Algorithm)
+
+  // Ek technique jo check karti hai ke Linked List mein koi cycle (loop) exist karti hai ya nahi — yani koi node wapas kisi purane node ki taraf point to nahi kar raha (jiski wajah se list kabhi null pe khatam na ho).
+
+  detectCycle() {
+    let slow = this.head;
+    let fast = this.head;
+
+    while(fast !== null && fast.next !== null) {
+      fast = fast.next.next;
+      slow = slow.next;
+
+      if(fast === slow)  {
+        console.log("Cycle Detected");
+        return true;
+      };
+      
+    };
+    return false;
+  };
+
+  //! Topic 4: Remove Duplicates from Linked List
+  // is topic main ham linked list ke duplicate values ko identify kare ge set ke help se or phir remove kar ke clean list nikale ge 
+
+
+  removeDuplicates() {
+    let seen = new Set(); // is main clean nodes store ho ge
+    let current = this.head; // filahl current head ha mean ke first node 
+    let prev = null; // or previous node abhi null ha link nahi kia 
+
+    // ab yaha se condition lage gi 
+
+    while (current !== null) {
+      if(seen.has(current.item)) {
+        prev.next = current.next; // yani ke agar seen main wo node pehle se jo abhi current item ha to us ko remove kar do or current ke next node ko prev ka next node bana do 
+
+        //? Edge Case ager tail node duplicate a jai to phir kia kare ge 
+
+        if(current.next === null) {
+          this.tail = prev; // yani agar current ka agle element null ho mean ke tail ho to phir prev node ko he tail bana do 
+        };
+      }
+      else {
+        seen.add(current.item) 
+        prev = current; // agar duplicate value nahi ha to us ko seen ke andar dal do or prev ko agey chala do 
+      }
+      current = current.next;
+    };
+  };
+
 }
 
 console.log("===Reverse A Linked List===");
@@ -110,3 +161,59 @@ list3.append("Sunny");
 list3.append("Waleed");
 
 console.log(list3.findMiddle()); // even list (4 elements) Middle Element = Sunny
+
+
+console.log("===Detect Cycle===");
+
+// Test 1: No cycle
+const list4 = new LinkedList();
+list4.append(10);
+list4.append(20);
+list4.append(30);
+console.log(list4.detectCycle()); // yaha per no cycle detected ai ga
+
+// Test 2: Cycle exists (manually banayenge)
+const list5 = new LinkedList();
+list5.append(10);
+list5.append(20);
+list5.append(30);
+list5.tail.next = list5.head; // yaha ab tail null nahi ha or wo head ke taraf point out kar raha ha is lia cycle detect ho jai ga or ture return ho ga
+
+console.log(list5.detectCycle()); // cycle detected and return  false
+
+
+console.log("===Remove Duplicates===");
+const list6 = new LinkedList();
+list6.append("Hammad");
+list6.append("Faizan");
+list6.append("Hammad");
+list6.append("Bilal");
+list6.append("Faizan");
+list6.append("Fahad");
+
+list6.removeDuplicates();
+
+let curr = list6.head;
+while (curr !== null) {
+  console.log(curr.item);
+  curr = curr.next;
+}
+
+console.log("Tail:", list6.tail.item); // Fahad Tail ho ga yaha 
+
+console.log("===Remove Duplicates Edge Case===");
+const list7 = new LinkedList();
+list7.append("Hammad");
+list7.append("Faizan");
+list7.append("Hammad");
+list7.append("Bilal");
+list7.append("Faizan");
+
+list7.removeDuplicates();
+let currRmEdge = list7.head;
+while(currRmEdge !== null) {
+  console.log(currRmEdge.item);
+  currRmEdge = currRmEdge.next;
+};
+
+console.log("Tail:", list7.tail.item); // Yaha per Bilal tail ho ga keuke faizan last node ha or duplicate ha is lia prev ko he tail bana dia ha 

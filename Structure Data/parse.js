0b03f57148283dff9932510817e8ce1ebb8d9424
Hammad <tanolihammad14@gmail.com>
2026-08-11 15:29:40 +0500

    Basics Of JSON Parse

---
+++ Structure Data/parse.js
@@ -0,0 +1,71 @@
+//! Topic 4 JSON.parse() — Concept
+
+// Converts a JavaScript Object Notation (JSON) string into an object or Array.
+// Basic Syntax
+// JSON.parse(value);
+
+// Example
+
+//? Json string banate hain phir us ko convert kare ge proper object main
+
+const jsonString = '{"name": "Hammad", "age": 22, "city": "Abbottabad"}';
+
+// ye ab json string ha ham dot notation se is ke value access nahi kar sakte
+
+console.log("===JSON String===");
+console.log(jsonString.name); // undefined
+console.log(typeof jsonString, jsonString); // String
+
+// Now Convert it into Ojbect and make it live so we can access the values with dot notation
+
+console.log("JSON String To Object===");
+const jsonParse = JSON.parse(jsonString);
+console.log(typeof jsonParse, jsonParse); // Object
+console.log(jsonParse.name); // Hammad cuz now it's live object so we can access key with notations
+
+//? Json array main convert karna
+
+const jsonArr = '[1, 5, 3, "Abbottabad", true, false, "Hammad", "Fahad"]';
+console.log("===JSON String of Array===");
+console.log(typeof jsonArr, jsonArr); // string
+console.log(Array.isArray(jsonArr)); // false keuke abhi ye string ha
+const parsedArr = JSON.parse(jsonArr);
+
+console.log(parsedArr[7]); // Fahad
+console.log(Array.isArray(parsedArr)); // true keuke ab ye array ha
+
+//! Parse Task
+
+//? Task One
+
+const jsonMovie = '{"name": "interstallar", "year": 2014, "rating": 9.5}';
+const parseMovie = JSON.parse(jsonMovie);
+
+console.log("===Task One===");
+console.log(parseMovie.name); // interstallar
+console.log(parseMovie.year); // 2014
+console.log(parseMovie.rating); // 9.5
+
+//? Task Two
+
+const studentsJSON = '[{"name":"Ali","marks":85},{"name":"Sara","marks":92}]';
+console.log("===Task Two===");
+const studentParse = JSON.parse(studentsJSON);
+
+console.log(studentParse[0].name);
+
+for (let student of studentParse) {
+  console.log(`${student.name} Scored ${student.marks}`);
+}
+
+
+//? Task Threee
+const nested =
+  '{"name": "Hammad", "address": {"city": "Abbottabad", "Province": "Kpk", "areas": ["Manshera", "Haripur", "Havaliann"]}}';
+
+  console.log("===Task Three===");
+  
+  const nestedParse = JSON.parse(nested);
+  console.log(nestedParse.address.areas[1]); // Haripur
+  console.log(nestedParse.address.areas); // manshera , haripur havalian
+  console.log(typeof nestedParse.address.areas); // object

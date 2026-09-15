2df2a341f6fdec29cacc7b581ced09a28b3d9d84
Hammad <tanolihammad14@gmail.com>
2026-09-15 16:49:48 +0500

    Multi Dimensional Arrays: Mini Project

--- Indexed Collection/multidimensionalarrays.js
+++ Indexed Collection/multidimensionalarrays.js
@@ -174,26 +174,20 @@ for (let i = 0; i < attendance.length; i++) {
 
 console.log("===Task 3===");
 
-let jagged = [
-  [5, 10],
-  [1, 2, 3, 4],
-  [100]
-];
+let jagged = [[5, 10], [1, 2, 3, 4], [100]];
 
-let jaggedTotal = 0; 
+let jaggedTotal = 0;
 
-for(let i = 0; i < jagged.length; i++) {
+for (let i = 0; i < jagged.length; i++) {
   for (let c = 0; c < jagged[i].length; c++) {
-    let current = jagged[i][c]; 
+    let current = jagged[i][c];
     jaggedTotal = jaggedTotal + current;
-  };
-};
+  }
+}
 
 console.log("Jagged Total:", jaggedTotal);
 
-
 //! Part G — Sub-topic 3: Basic Matrix Operations
-
 
 //?  Operation 1: Row-wise Sum
 
@@ -201,59 +195,52 @@ console.log("Jagged Total:", jaggedTotal);
 console.log("===Basic Matrix Operations===");
 
 let matrix3 = [
-  [1, 2, 3], 
-  [4, 5, 6], 
-  [7, 8, 9]
-]; 
-
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
 
 for (let i = 0; i < matrix3.length; i++) {
-  let rowSum = 0; 
+  let rowSum = 0;
   for (let c = 0; c < matrix3[i].length; c++) {
-    let current = matrix3[i][c]; 
+    let current = matrix3[i][c];
     rowSum = rowSum + current;
-  }; 
-  console.log(`Row: ${i} sum: ${rowSum}`); 
-};
+  }
+  console.log(`Row: ${i} sum: ${rowSum}`);
+}
 
 //?  Operation 2: Column-wise Sum
 
-// Uper matrix 3 ko he use karte hain 
+// Uper matrix 3 ko he use karte hain
 
+// is main ham loop ka order reverse kar de ga pehle row outer loop per traverse hoti thi ab outer loop per column ko traverse kare ga
 
-// is main ham loop ka order reverse kar de ga pehle row outer loop per traverse hoti thi ab outer loop per column ko traverse kare ga 
- 
 for (let col = 0; col < matrix3[0].length; col++) {
-  let colSum = 0; // start main col sum 0 ho ga 
+  let colSum = 0; // start main col sum 0 ho ga
 
   for (let row = 0; row < matrix3.length; row++) {
-    let current = matrix3[row][col]; 
+    let current = matrix3[row][col];
     colSum = colSum + current;
-  };
+  }
 
   console.log(`Column: ${col} sum: ${colSum}`);
-};
-
+}
 
 //! Operation 3: Transpose (Rows ↔ Columns swap)
 
 // Transpose matlab: matrix ko is tarah ghumana ke rows, columns ban jayein aur columns, rows.
 
+// new empty matrix array banaya same size ka
 
-
-// new empty matrix array banaya same size ka 
-
-let transposed = Array.from({length: 3}, () => new Array(3).fill(0)); 
-
+let transposed = Array.from({ length: 3 }, () => new Array(3).fill(0));
 
 for (let i = 0; i < matrix3.length; i++) {
-  for(let c = 0; c < matrix3.length; c++) {
+  for (let c = 0; c < matrix3.length; c++) {
     transposed[c][i] = matrix3[i][c]; // yaha per trasposed ke row ko colum se badal dia or column ko row se
-  };
-};
+  }
+}
 
 console.log(transposed);
-
 
 // ! Practice Tasks — Matrix Operations
 
@@ -263,24 +250,23 @@ console.log("===Task One===");
 
 let matrix4 = [
   [23, 42, 59],
-  [45, 65, 70], 
-  [98, 95, 46]
+  [45, 65, 70],
+  [98, 95, 46],
 ];
 
 let rowSums = [];
 
 for (let i = 0; i < matrix4.length; i++) {
   let total = 0;
-  for(let c = 0; c < matrix4[i].length; c++){
-  let current = matrix4[i][c];
-  total = total + current;
-  }; 
+  for (let c = 0; c < matrix4[i].length; c++) {
+    let current = matrix4[i][c];
+    total = total + current;
+  }
 
   rowSums.push(total);
 }
 
 console.log(rowSums);
-
 
 // Task 2: Column Sums Array
 
@@ -288,31 +274,28 @@ console.log("===Task Two===");
 
 let colSums = [];
 
-for(let col = 0; col < matrix4[0].length; col++) {
-  // is main outer array main columns ko itreate kia ha 
+for (let col = 0; col < matrix4[0].length; col++) {
+  // is main outer array main columns ko itreate kia ha
 
   let total = 0;
-  for(let row = 0; row < matrix4.length; row++) {
+  for (let row = 0; row < matrix4.length; row++) {
     let current = matrix4[row][col];
     total = total + current;
   }
   colSums.push(total);
-}; 
+}
 
 console.log(colSums);
 
-
 //? Task 3: Transpose Function
 
-
-console.log("===Task Three==="); 
-
+console.log("===Task Three===");
 
 function trasposeMatrix(matrix) {
-  let size = matrix.length; 
-  let transposed = Array.from({length: size}, () => new Array(size));
+  let size = matrix.length;
+  let transposed = Array.from({ length: size }, () => new Array(size));
 
-  //? Nested Loop 
+  //? Nested Loop
 
   for (let i = 0; i < size; i++) {
     for (let c = 0; c < size; c++) {
@@ -320,60 +303,122 @@ function trasposeMatrix(matrix) {
     }
   }
   return transposed;
-}; 
+}
 
 console.log(trasposeMatrix(matrix4));
 
-
 //! Combined Practice Tasks Part G
-
 
 //? Task 1: Diagonal Sum
 
-
 console.log("===Task One===");
 
-
 const diagonalMatrix = [
-  [1, 2, 3], 
-  [4, 5, 6], 
-  [7, 8, 9]
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
 ];
 
 console.log("===Task One===");
 
-let diagonalSum = 0; 
+let diagonalSum = 0;
 
 for (let i = 0; i < diagonalMatrix.length; i++) {
-  let current = diagonalMatrix[i][i]; 
+  let current = diagonalMatrix[i][i];
   diagonalSum = diagonalSum + current;
-
-}; 
+}
 
 console.log(diagonalSum);
 
-
 //? Task 2: Find Max in Matrix
 
-console.log("===Task Two==="); 
-
+console.log("===Task Two===");
 
 const maxMatrix = [
   [93, 59, 30],
-  [90, 40, 20], 
-  [98, 99, 10]
-]; 
+  [90, 40, 20],
+  [98, 99, 10],
+];
 
-
-let maxSoFor = maxMatrix[0][0]; // start with the first element 
+let maxSoFor = maxMatrix[0][0]; // start with the first element
 
 for (let row = 0; row < maxMatrix.length; row++) {
   for (let col = 0; col < maxMatrix[row].length; col++) {
     let current = maxMatrix[row][col];
-    if(current > maxSoFor) {
-      maxSoFor = current; 
+    if (current > maxSoFor) {
+      maxSoFor = current;
     }
   }
 }
 
-console.log(maxSoFor); 
+console.log(maxSoFor);
+
+//? Task 3: Matrix Equality Check
+
+console.log("===Task Three====");
+
+function areMatrixEqual(a, b) {
+  let isEqual = true;
+
+  for (let i = 0; i < a.length; i++) {
+    for (let c = 0; c < a[i].length; c++) {
+      if (a[i][c] !== b[i][c]) {
+        isEqual = false; // Mean ke agar 1 bhi mismatch mila to false return kar do
+      }
+    }
+  }
+  return isEqual;
+}
+
+console.log(areMatrixEqual(matrix1, matrix2)); // false cuz both are not same
+
+let mat1 = [
+  [1, 2],
+  [3, 4],
+];
+
+let mat2 = [
+  [1, 2],
+  [3, 4],
+];
+
+console.log(areMatrixEqual(mat1, mat2)); // true cuz the both are same
+
+//! Mini Project
+
+let marks = [
+  [85, 90, 78, 92], // Student 0
+  [70, 65, 80, 75], // Student 1
+  [95, 88, 91, 89], // Student 2
+];
+
+console.log("===Mini Project===");
+
+let studentTotal = [];
+
+for (let i = 0; i < marks.length; i++) {
+  let total = 0;
+  for (let c = 0; c < marks[i].length; c++) {
+    // step one abhi ham yaha her row main her student ke marks ko total kar ke student total array main push kare ge
+
+    let current = marks[i][c];
+    total = total + current;
+  }
+  studentTotal.push(total); // first step done
+
+}
+
+
+//? Step Two Avarage of each student
+let studentPercentage = [];
+
+for (let i = 0; i < studentTotal.length; i++) {
+  let percentage = (studentTotal[i] / 400) * 100;
+  studentPercentage.push(percentage); // step two complete
+}
+
+console.log("===Student Total Step One===");
+console.log(studentTotal);
+
+console.log("===Step Two Percentage of Each Student===");
+console.log(studentPercentage);
